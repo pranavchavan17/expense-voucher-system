794bffdda8a588aad6d800c3d97faebb0a229253
pranavchavan17 <pranavc671@gmail.com>
2026-07-19 10:18:43 +0530

    Complete Module 1: Initial project setup and MySQL configuration

---
+++ src/test/java/com/pspl/expense_voucher_system/ExpenseVoucherSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pspl.expense_voucher_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseVoucherSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
